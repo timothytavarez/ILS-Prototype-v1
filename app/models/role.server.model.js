897858b6ac34b260c2a622d075a1e28371cd2c62
Nathan Rice <thanatos454@gmail.com>
2015-07-31 19:54:05 -0600

    Improved the model for roles.  Added in roleName, created, and updated.
    Also reduced the rights properties.  Now there is only a property per
    catagory of rights.  Each category is set to the type of Mixed which
    means it can be specified at runtime.
    
    This allows us to populate each role document with only the properties
    they supply to the user account; instead of a huge list of boolean
    properties set to false.

--- app/models/role.server.model.js
+++ app/models/role.server.model.js
@@ -10,85 +10,158 @@ var mongoose = require('mongoose'),
  * Role Schema
  */
 var RoleSchema = new Schema({
-  // Muse Employee
-  museLogin: Boolean,
-  editAllOrganizations: Boolean,
-  viewAllOrganizations: Boolean,
-  browseSystemItems: Boolean,
 
-  // Librarians
-  staffLogin: Boolean,
+	roleName: {
+		type: String,
+		required: true,
+		trim: true
+	},
+	
+	created: { 
+		type: Date,
+		default: Date.now
+	},
 
-  // Librarians - Circulate
-  checkOut: Boolean,
-  checkIn: Boolean,
-  sell: Boolean,
-  forceCheckout: Boolean, // Allow checkout if a limitation exists
-  forceRenewal: Boolean, // Allow renewal if a limitation exists
-  forceReserve: Boolean,
-  forceSell: Boolean,
-  editReservePriority: Boolean,
-  manageRestrictions: Boolean,
-  generateOverdueReport: Boolean,
+	updated: {
+		type: Date,
+		default: Date.now
+	},
 
-  // Librarians - Administration
-  editOrganizationSettings: Boolean,
-  editOrganizationCalendar: Boolean,
-  editOrganizationAnnouncements: Boolean, // Viewable once logged in as a staff member
-  editOrganizationNews: Boolean, // Viewable publicly/by patrons
-  editQuoteOfTheDay: Boolean, // Edit queued quote of the day (If enabled in settings)
-  scheduleStaffNewsletter: Boolean, // Emailed
-  schedulePatronNewsletter: Boolean,
-  emailPatron: Boolean,
-  manageCirculationRules: Boolean,
-  manageNotificationRules: Boolean, // Triggers & notice message
-  editStaffAccounts: Boolean, // Includes permissions
-  viewStaffAccounts: Boolean,
-  viewLogs: Boolean,
-  exportLogs: Boolean,
-  viewReports: Boolean,
 
-  // Librarians - Borrower Management
-  addPatrons: Boolean,
-  editPatrons: Boolean,
-  editPatronComments: Boolean,
-  editPatronTags: Boolean,
-  updateCharges: Boolean,
-  writeOffCharges: Boolean,
-  bulkImportPatrons: Boolean,
-  bulkEditPatrons: Boolean,
+  // Muse Employee Rights
+	godRights: Schema.Types.Mixed,
+  // editAllOrgs: Boolean,
+  // viewAllOrgs: Boolean,
+  // browseSystemItems: Boolean,
 
-  // Librarian - Catalog Management
-  editCatalogBibliographic: Boolean, // Data about a item (i.e. title)
-  editCatalogHolding: Boolean,
-  editCatalogItem: Boolean, // i.e. add a new book or mark a book as for sell, tags
-  editSubscribedSerials: Boolean,
-  viewSubscribedSerials: Boolean,
-  addMissingSerial: Boolean,
-  addReceivedSerials: Boolean,
-  massEditCatalogHoldings: Boolean,
-  massEditCatalogItems: Boolean,
-  exportCatalog: Boolean,
-  createLabel: Boolean, // printable labels/cards for patrons
-  createBarcode: Boolean, // Printable barcodes from catalog
+  // Login Access Rights
+	loginRights: Schema.Types.Mixed,
+  // devLogin: Boolean,
+  // staffLogin: Boolean,
+	// patronLogin: Boolean,
 
-  // Librarian - Acquisitions
-  viewBudget: Boolean,
-  editBudget: Boolean,
-  addBudgetItem: Boolean,
-  editBudgetItem: Boolean,
-  viewBudgetItems: Boolean,
-  addVendor: Boolean,
-  editVendor: Boolean,
-  addOrder: Boolean,
-  editOrder: Boolean,
-  receiveOrder: Boolean, // Can receive item or entire order
-  viewOrderReports: Boolean,
+	
+  // Base Circulation Rights
+	baseCircRights: Schema.Types.Mixed,
+  // checkOut: Boolean,
+  // checkIn: Boolean,
+  // sell: Boolean,
+	
+  // Extended Circulation Rights
+	extCircRights: Schema.Types.Mixed,
+  // forceCheckout: Boolean, // Allow checkout if a limitation exists
+  // forceRenewal: Boolean, // Allow renewal if a limitation exists
+  // forceReserve: Boolean,
+  // forceSell: Boolean,
+  // editReservePriority: Boolean,
+  // manageRestrictions: Boolean,
+  // generateOverdueReport: Boolean,
+  // manageCirculationRules: Boolean,
 
-  // Patron
-  patronLogin: Boolean,
-  reserve: Boolean,
-  search: Boolean,
+  // Base Administrative Rights
+	baseAdminRights: Schema.Types.Mixed,
+  // manageNotificationRules: Boolean, // Triggers & notice message
+  // editOrgCalendar: Boolean,
+  // editOrgAnnouncements: Boolean, // Viewable once logged in as a staff member
+  // editOrgNews: Boolean, // Viewable publicly/by patrons
+  // editQuoteOfTheDay: Boolean, // Edit queued quote of the day (If enabled in settings)
+  // schedulePatronNewsletter: Boolean,
+  // emailPatron: Boolean,
 
-  user: {type: Schema.ObjectId, ref: 'User'} // assuming you name your model User
+	// Extended Administrative Rights
+  extAdminRights: Schema.Types.Mixed,
+  // editOrgSettings: Boolean,
+  // scheduleStaffNewsletter: Boolean, // Emailed
+  // editStaffAccounts: Boolean, // Includes permissions
+  // viewStaffAccounts: Boolean,
+  
+  // viewLogs: Boolean,
+  // exportLogs: Boolean,
+	
+	// Base Reporting Rights
+	baseReportRights: Schema.Types.Mixed,
+	// launchReports: Boolean,
+	// viewReportResults: Boolean,
+	// downloadReportResults: Boolean,
+	
+	// Extended Reporting RIghts
+	extReportRights: Schema.Types.Mixed,
+	// createReportTemplates: Boolean,
+	// deleteReportTemplates: Boolean,
+	// importReportTemplates: Boolean,
+	// exportReportTemplates: Boolean,
+
+  // Base Patron Management Rights
+	basePatronRights: Schema.Types.Mixed,
+  // addPatrons: Boolean,
+  // editPatrons: Boolean,
+  // editPatronComments: Boolean,
+  // editPatronTags: Boolean,
+	
+	// Extended Patron Management Rights
+	extPatronRights: Schema.Types.Mixed,
+  // updateCharges: Boolean,
+  // writeOffCharges: Boolean,
+  // bulkImportPatrons: Boolean,
+  // bulkEditPatrons: Boolean,
+
+  // Base Cataloging Rights
+	baseCatalogRights: Schema.Types.Mixed,
+	// createItem: Boolean,
+  // editItemData: Boolean, // Data about an item (i.e. title)
+  // editItemHolding: Boolean, // Data about current status of the item(ie, avialable, lost, long overdue, lost and paid for, missing in Inventory, or Missing in Hold Queue)
+  // editItemTag: Boolean, // i.e. mark a book as for sell, lost, and other such tags
+
+	// Extended Cataloging Rights
+	extCatalogRights: Schema.Types.Mixed,
+  // massEditCatalogHoldings: Boolean,
+  // massEditCatalogItems: Boolean,
+  // exportCatalog: Boolean,
+
+	// Base Serial Cataloging Rights
+	baseSerialRights: Schema.Types.Mixed,
+  // editSubscribedSerials: Boolean,
+  // viewSubscribedSerials: Boolean,
+  // addMissingSerial: Boolean,
+  // addReceivedSerials: Boolean,
+
+	// Base Labeling Rights
+	baseLabelRights: Schema.Types.Mixed,
+  // createLabel: Boolean, // printable labels/cards for patrons
+	// editLabel: Boolean,
+	// deleteLabel: Boolean,
+
+	// Base Barcoding Rights
+	baseBarcodeRights: Schema.Types.Mixed,
+  // createBarcode: Boolean, // Printable barcodes from catalog
+	// editBarcode: Boolean,
+	// deleteBarcode: Boolean,
+
+  // Read Budget Rights
+	readBudgetRights: Schema.Types.Mixed,
+  // viewBudget: Boolean,
+  // viewBudgetItems: Boolean,
+  // viewOrderReports: Boolean,
+
+	// Write Budget Rights
+	writeBudgetRights: Schema.Types.Mixed,
+  // receiveOrder: Boolean, // Can receive item or entire order
+  // addVendor: Boolean,
+  // addOrder: Boolean,
+  // addBudgetItem: Boolean,
+  // editOrder: Boolean,
+  // editBudget: Boolean,
+  // editBudgetItem: Boolean,
+  // editVendor: Boolean,
+	// TODO: deletion Rights?
+});
+
+
+
+/**
+ * Hook a pre save method to keep the updated property current
+ */
+RoleSchema.pre('save', function(next) {
+	this.updated = Date.now;
+	next();
 });
