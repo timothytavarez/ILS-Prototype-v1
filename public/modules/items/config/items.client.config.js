68cde6e385c4228d794a519183678cb5c798afcf
Timothy Tavarez <timothy.tavarez@eonfirm.com>
2015-08-02 22:04:53 +0000

    Adding eeh-nav html

--- public/modules/items/config/items.client.config.js
+++ public/modules/items/config/items.client.config.js
@@ -4,7 +4,7 @@
 angular.module('items').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Items', 'items', 'dropdown', '/items(/create)?');
+		Menus.addMenuItem('topbar', 'Items', 'items', 'dropdown', '/items');
 		Menus.addSubMenuItem('topbar', 'items', 'List Items', 'items');
 		Menus.addSubMenuItem('topbar', 'items', 'New Item', 'items/create');
 	}
