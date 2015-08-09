a408bcc87a12dc9f8be5e3d556f29504fa945ff2
Timothy Tavarez <timothy.tavarez@eonfirm.com>
2015-08-09 22:09:47 +0000

    Modified Muse view routing to use ui-view

--- public/modules/core/controllers/sidebar.client.controller.js
+++ public/modules/core/controllers/sidebar.client.controller.js
@@ -3,6 +3,7 @@
 
 angular.module('core').controller('SidebarController', ['$scope', 'Authentication',
     function($scope, Authentication) {
+        
     // This provides Authentication context.
     $scope.authentication = Authentication;
     }
