464d9dc6721c5a777bda0eb697c37468e73fbf04
Nathan Rice <thanatos454@gmail.com>
2015-08-08 18:31:01 -0600

    Fixed the issue with the topbar navigation bar menu not dropping down.  An updated to ui-bootstrap had necessitated the additon of dropdown and dropdown-toggle directives be added to both the menu item's wrapper and the menu link itself respectively.

--- public/modules/items/config/items.client.config.js
+++ public/modules/items/config/items.client.config.js
@@ -5,7 +5,7 @@ angular.module('items').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
 		Menus.addMenuItem('topbar', 'Items', 'items', 'dropdown', '/items');
-		Menus.addSubMenuItem('topbar', 'items', 'List Items', 'items');
-		Menus.addSubMenuItem('topbar', 'items', 'New Item', 'items/create');
+		Menus.addSubMenuItem('topbar', 'items', 'List Items', 'listItems');
+		Menus.addSubMenuItem('topbar', 'items', 'New Item', 'createItem');
 	}
 ]);
