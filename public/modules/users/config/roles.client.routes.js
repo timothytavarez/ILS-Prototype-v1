4b435091a77ae1505bc5e06c6f9e2575c8d0a038
Nathan Rice <thanatos454@gmail.com>
2015-08-10 18:37:55 -0600

    Fixed the routes contained in roles.client.routes.js.  They now specify which views the partials will load to.

--- public/modules/users/config/roles.client.routes.js
+++ public/modules/users/config/roles.client.routes.js
@@ -7,19 +7,47 @@ angular.module('users').config(['$stateProvider',
 		$stateProvider.
 		state('listRoles', {
 			url: '/roles',
-			templateUrl: 'modules/users/views/list-roles.client.view.html'
+			views: {
+			  'contentView': {
+					templateUrl: 'modules/users/views/list-roles.client.view.html'
+			  },
+			  'sidebarView' : {
+					templateUrl: 'modules/core/views/sidebar.client.view.html' 
+			  }
+			}
 		}).
 		state('createRole', {
 			url: '/roles/create',
-			templateUrl: 'modules/users/views/create-role.client.view.html'
+			views: {
+			  'contentView': {
+					templateUrl: 'modules/users/views/create-role.client.view.html'
+			  },
+			  'sidebarView' : {
+					templateUrl: 'modules/core/views/sidebar.client.view.html' 
+			  }
+			}
 		}).
 		state('viewRole', {
 			url: '/roles/:roleId',
-			templateUrl: 'modules/users/views/view-role.client.view.html'
+			views: {
+			  'contentView': {
+					templateUrl: 'modules/users/views/view-role.client.view.html'
+			  },
+			  'sidebarView' : {
+					templateUrl: 'modules/core/views/sidebar.client.view.html' 
+			  }
+			}
 		}).
 		state('editRole', {
 			url: '/roles/:roleId/edit',
-			templateUrl: 'modules/users/views/edit-role.client.view.html'
+			views: {
+			  'contentView': {
+					templateUrl: 'modules/users/views/edit-role.client.view.html'
+			  },
+			  'sidebarView' : {
+					templateUrl: 'modules/core/views/sidebar.client.view.html' 
+			  }
+			}
 		});
 	}
 ]);
